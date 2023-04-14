d45560d07a428bc66494de2ed16c6cc58c3aa9eb
Walter Smith <walter.c.smith01@gmail.com>
2023-04-14 15:55:40 -0500

    Created another HTML page, added some elements to B2BTitle HTML page, and conducted a basic console out put on the BasicTechniques java page

--- src/main/java/com/back2basics/BasicTechniques.java
+++ src/main/java/com/back2basics/BasicTechniques.java
@@ -4,6 +4,10 @@ public class BasicTechniques {
     public static void main(String[] args){
         System.out.println("What's Good?!, I'm back!");
 
+        int myFavoriteNumber;
+        myFavoriteNumber = 308;
+        System.out.print(myFavoriteNumber);
+
     }
 }
 
