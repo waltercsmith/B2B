7577d082488bbbff4f20e7a117b113027ebe2726
Walter Smith <walter.c.smith01@gmail.com>
2023-04-17 14:30:22 -0500

    Used JavaScript to interact with HTML: Prompts,Confirm, Alerts

---
+++ src/main/HTML.CSS.images.js/HTML/JS/Alerts.Confirms.Prompts.js
@@ -0,0 +1,18 @@
+"use strict"
+var confirm = confirm('Yes or No');
+
+//A console log message
+console.log('Hello from B2B! (Beverages & Beats)');
+
+
+//A Welcome alert
+alert('Hello from B2B! (Beverages & Beats)');
+
+
+
+// console log message will print user input
+
+var userInput = prompt('What\'s your favorite beverage currently?');
+
+console.log(userInput);
+
